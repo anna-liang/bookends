cd0d812e152e58402b723d0a4108247dfba10c5d
anna <annaliang56@gmail.com>
2026-01-30 19:37:02 -0500

    Map Google volume to book

---
+++ frontend/lib/mappers/books.ts
@@ -0,0 +1,23 @@
+import { GoogleVolume } from '@/types/external/googleBooks';
+import { Book } from '@/types/books';
+
+export function mapGoogleVolumeToBook(googleVolumes: GoogleVolume[]): Book[] {
+  const books: Book[] = googleVolumes.map((volume) => ({
+    id: volume.id,
+    title: volume.volumeInfo.title,
+    authors: volume.volumeInfo.authors ?? [],
+    publisher: volume.volumeInfo.publisher,
+    publishedDate: volume.volumeInfo.publishedDate,
+    description: volume.volumeInfo.description,
+    pageCount: volume.volumeInfo.pageCount,
+    mainCategory: volume.volumeInfo.mainCategory,
+    categories: volume.volumeInfo.categories,
+    averageRating: volume.volumeInfo.averageRating,
+    ratingsCount: volume.volumeInfo.averageRating,
+    image: volume.volumeInfo.imageLinks
+      ? volume.volumeInfo.imageLinks.thumbnail
+      : '',
+    previewLink: volume.volumeInfo.previewLink,
+  }));
+  return books;
+}
