d1d7461af121c237d16a36162d358153386156e9
Anna Liang <annaliang56@gmail.com>
2026-02-17 14:32:38 -0500

    Update search bar

--- frontend/app/search/searchBar.tsx
+++ frontend/app/search/searchBar.tsx
@@ -4,32 +4,34 @@ import { useState } from 'react';
 import { Book } from '../../types/books';
 import BookList from '@/components/list/bookList';
 import { getBooks } from '@/api/booksService';
+import { InputGroup } from "@heroui/react";
+import { SearchSm } from '@untitledui/icons'
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
   const [books, setBooks] = useState<Book[]>([]);
-  const handleSearch = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const books = await getBooks(query);
-      setBooks(books);
-    } catch (err) {
-      console.error(err);
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        const books = await getBooks(query);
+        setBooks(books);
+      } catch (err) {
+        console.error(err);
+      }
     }
   };
 
   return (
     <div>
-      <div className="flex h-50 items-center justify-center">
-        <form onSubmit={handleSearch}>
-          <input
-            type="text"
-            placeholder="Search books..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button type="submit">Search</button>
-        </form>
+      <div className="flex h-30 w-80 items-center justify-center">
+        <InputGroup fullWidth variant='primary' className='text-grey-400 bg-transparent rounded-sm'>
+          <InputGroup.Input placeholder="Search books..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleSearch} />
+          <InputGroup.Suffix>
+            <SearchSm className='size-5' />
+          </InputGroup.Suffix>
+        </InputGroup>
       </div>
       <BookList books={books} />
     </div>
