a5888a49e5658a59797b63d556df1d1836c2cb86
Anna Liang <annaliang56@gmail.com>
2026-02-17 13:57:30 -0500

    Use remotePatterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['books.google.com'],
+    remotePatterns: [{
+      protocol: 'http',
+      hostname: 'books.google.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+      port: '',
+      pathname: '/**'
+    }]
   },
 };
 
