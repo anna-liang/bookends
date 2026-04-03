bd4b1d1f6d52f8bab12600695fc6c7a652d84428
Anna Liang <annaliang56@gmail.com>
2026-04-03 17:11:40 -0400

    Change port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import app from './app.ts';
 import { connectRedis } from './redisClient.ts';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
   try {
